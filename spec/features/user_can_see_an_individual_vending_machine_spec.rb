2bdd60b4b294ed069f20c49a57ac691d5485b755
Ben Lee <bendelonlee@gmail.com>
2018-12-21 10:18:46 -0700

    Model tests passing

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -2,11 +2,24 @@ require 'rails_helper'
 
 feature 'When a user visits a vending machine show page' do
   scenario 'they see the location of that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    owner = Owner.create!(name: "Sam's Snacks")
+    machine  = owner.machines.create!(location: "Don's Mixed Drinks")
+
+    snack_1 = Snack.create!(name: "Chips", price: 2)
+    snack_2 = Snack.create(name: "Soda", price: 3)
+
+    machine.snacks += [snack_1, snack_2]
 
     visit machine_path(dons)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+
+    within "#snack-#{snack.id}" do
+      expect(page).to have_content(snack_1.name)
+      expect(page).to have_content(snack_1.price)
+    end
+
   end
 end
+
+#I see the name of all of the snacks associated with that vending machine along with their price
--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
-describe Owner, type: :model do
+describe Snack, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :price }
     it { should have_many :machines }
+    it { should have_many :snack_machines }
   end
 end
