2a7d8a6edf676d2b7fe60d9642b6f220b3440ca1
Ben Lee <bendelonlee@gmail.com>
2018-12-21 10:49:22 -0700

    US3

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -7,8 +7,20 @@ feature 'When a user visits a vending machine show page' do
 
     snack_1 = Snack.create!(name: "Chips", price: 2)
     snack_2 = Snack.create(name: "Soda", price: 4)
+    snack_3 = Snack.create!(name: "sdf", price: 2)
+    snack_4 = Snack.create!(name: "sdffsd", price: 2)
+
 
     machine.snacks += [snack_1, snack_2]
+
+    machine_2  = owner.machines.create!(location: "AMC")
+    machine_2.snacks += [snack_1, snack_3, snack_4]
+
+    machine_3  = owner.machines.create!(location: "RNC")
+    machine_3.snacks << snack_2
+
+
+
 
     visit machine_path(machine)
 
@@ -17,18 +29,24 @@ feature 'When a user visits a vending machine show page' do
     within "#snack-#{snack_1.id}" do
       expect(page).to have_content(snack_1.name)
       expect(page).to have_content("Price: #{snack_1.price}")
+      expect(page).to have_content(machine_2.location)
+      expect(page).to have_content("Average Price: 2")
+      expect(page).to have_content("Number of Unique Snacks: 3")
     end
 
     within "#snack-#{snack_2.id}" do
       expect(page).to have_content(snack_2.name)
       expect(page).to have_content("Price: #{snack_2.price}")
+      expect(page).to have_content(machine_3.location)
+      expect(page).to have_content("Average Price: 4")
+      expect(page).to have_content("Number of Unique Snacks: 1")
+
     end
 
 
     expect(page).to have_content "Average Price of Machine: 3"
-    #I also see an average price for all of the snacks in that machine
 
-
+ # and I see a count of the different kinds of items in that vending machine.
   end
 end
 
